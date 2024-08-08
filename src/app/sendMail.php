c29ea8aeb93a6d090f92a9ca3412dab8004230b6
Besim91 <78695114+Besim91@users.noreply.github.com>
2024-08-08 15:15:27 +0200

    Change mail adress

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'besimnaim@gmail.com';  
+            $recipient = 'contact@besimmustafi.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
